51431930f3699c43c8a2d61687e81916e7f8cbff
uboal <uboal@student.kit.edu>
2025-07-24 15:11:30 +0200

    removed unneeded test

--- tests/test-2.spec.ts
+++ tests/test-2.spec.ts
@@ -1,18 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  await page.goto('https://lecture-translator.kit.edu/');
-  await page.getByRole('link', { name: 'Login Login' }).click();
-  await page.getByRole('button', { name: 'Log in with KIT-Account (' }).click();
-  await page.getByRole('textbox', { name: 'Username:' }).fill('uboal');
-  await page.getByRole('textbox', { name: 'Password:' }).click();
-  await page.getByRole('textbox', { name: 'Password:' }).fill('3436.nevizadE!');
-  await page.getByRole('textbox', { name: 'Password:' }).press('Enter');
-  await page.getByRole('button', { name: 'Login', exact: true }).click();
-  await page.getByRole('link', { name: 'Live event Live event' }).click();
-  await page.getByRole('img', { name: 'Start Event' }).click({
-    button: 'right'
-  });
+
   await page.locator('div').filter({ hasText: 'Start Event' }).nth(3).click();
   await page.getByText('Lecture Name Accept Terms of').click();
   await page.getByRole('button', { name: 'Advanced Options' }).click();
